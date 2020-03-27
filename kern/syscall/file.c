8aa6311ad1db75ccde30a36626b22c0a45c7d338
Troy Murray <z5118366@ad.unsw.edu.au>
2020-03-27 16:32:38 +1100

    Implemented sys_write

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -293,7 +293,52 @@ ssize_t sys_read(int fd, userptr_t buf, size_t buflen) {
 
 /* Write data to file */
 ssize_t sys_write(int fd, userptr_t buf, size_t nbytes) {
-    // TODO
+
+    /* First we need to check that the fd is a valid file handle */
+    if (fd < 0 || fd >= OPEN_MAX) {
+        return EBADF;
+    }
+
+    /* Then we need to match it to an entry in the open file table, and check that the entry has been opened*/
+    int oftIndex = curproc->p_fdt[fd];
+    if (oftIndex == -1) {
+        return EBADF;
+    }
+
+    /* Copy in the user's pointer */
+    void *safeBuff[bufflen];
+    int result = copyin(buf, safeBuff, buflen);
+    if (result) {
+        return result;
+    }
+
+
+    /* Next we acquire the lock for the open file table, as we are about to modify the file pointer */
+    lock_acquire(oft->oftLock);
+
+    /* Then we load the current offset (file pointer) */
+
+    int offset = oft->oftArray[oftIndex]->fp;
+
+    /* Next create the uio variable that needs to be passed to VOP_READ*/
+    struct iovec iov;
+    struct uio u;
+    uio_uinit(iov, u, safeBuff, buflen, offset, UIO_USERSPACE);
+
+    /* Call the VOP_READ macro*/
+    result = VOP_WRITE(oft->oftArray[oftIndex]->vn, u)
+    if (result) {
+        return result;
+    }
+
+    /* Update the offset (file pointer) */
+    oft->oftArray[oftIndex]->fp = u->uio_offset;
+
+    /* Release the lock */
+    lock_release(oft->oftLock);
+
+    /* Return the amount read*/
+    return u->uio_resid;
 }
 
 /* Change current position in file */
